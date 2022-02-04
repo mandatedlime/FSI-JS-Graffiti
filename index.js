35e12d32e3bbb4fda1fd1cb4f990503318ecbd20
Joram Obbus <joramobbus@gmail.com>
2022-02-03 19:48:57 -0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 let header = document.querySelector('#page-header')
 header.style.textAlign = "left"
+header.style.backgroundColor = "gray"
+header.style.color = "white"
+
 let dogImages = document.querySelectorAll(".dog-image")
 for(let i = 0; i < dogImages.length; i++){
     dogImages[i].style.borderRadius = "50px"
 }
 
+
+
 let footer = document.querySelector(".footer")
 footer.style.borderStyle = "solid"
+footer.style.backgroundColor = "gray"
+footer.style.color = "white"
 
 let dogDescription = document.querySelectorAll(".dog-description")
 for(let i = 0; i < dogDescription.length; i++){
